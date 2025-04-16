a635a42cb626fbf999cc9bf0a830426a83a8daba
Mukul1209960 <mukulk386@gmail.com>
2025-04-16 21:06:07 +0530

    2ndCommit

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const RegisterForm = () => {
   const[formValues,setFormValues] = useState({
@@ -8,6 +9,57 @@ const RegisterForm = () => {
     mobile:"",
     checkbox: false,
   });
+  const[errors,setErrors] = useState({
+    name:null,
+    username:null,
+    email:null,
+    mobile:null,
+    checkbox:null,
+  });
+  const navigate = useNavigate();
+  const handleSignUp = () =>{
+    let isErrors = false;
+    if(formValues.name.trim().length === 0){
+      setErrors((prev) => ({...prev, name: "Name is Required" }));
+      isErrors = true;
+    }
+    else {
+      setErrors((prev)=>({...prev , name:null}))
+    }
+    if(formValues.username.trim().length === 0){
+      setErrors((prev)=>({...prev,username:"Username is Required"}));
+      isErrors = true;
+    }
+    else {
+      setErrors((prev)=>({...prev,username:null}))
+    }
+    if(formValues.email.trim().length === 0){
+      setErrors((prev)=>({...prev,email:"Email is Required"}));
+      isErrors = true;
+    }
+    else {
+      setErrors((prev)=>({...prev,email:null}))
+    }
+    if(formValues.mobile.trim().length === 0){
+      setErrors((prev)=>({...prev,mobile:"Mobile is Required"}));
+      isErrors = true;
+    }
+    else {
+      setErrors((prev)=>({...prev,mobile:null}))
+    }
+    if(formValues.checkbox === false){
+      setErrors((prev)=>({...prev,checkbox: "Please Accept the terms and conditions"}));
+      isErrors = true;
+    }
+    else {
+      setErrors((prev)=>({...prev,checkbox:null}))
+    }
+    console.log(isErrors,"isErrors");
+    if(isErrors === false) {
+      alert("Registered Successfully");
+      navigate("/Movies");
+    };
+  };
   const handleChange = (e) =>{
     setFormValues({...formValues, [e.target.name]: e.target.value });
   };
@@ -17,17 +69,22 @@ const RegisterForm = () => {
       <h1 className="text-4xl font-bold text-yellow-300">Super App</h1>
       <h2 className="text-2xl font-semibold text-yellow-300">Create your Account</h2>
     <input type="text" placeholder="Name" value={formValues.name} onChange={handleChange} name="name"/>
+    {errors.name ? <p className="text-red-700">{errors.name}</p> : <></>}
     <input type="text" placeholder="userName" value={formValues.username} onChange={handleChange} name="username"/>
+    {errors.username ? <p className="text-red-700">{errors.username}</p> : <></>}
     <input type="email" placeholder="Email" value={formValues.email} onChange={handleChange} name="email"/>
+    {errors.email ? <p className="text-red-700">{errors.email}</p> : <></>}
     <input type="text" placeholder="mobile" value={formValues.mobile} onChange={handleChange}name="mobile"/>
+    {errors.mobile ? <p className="text-red-700">{errors.mobile}</p> : <></>}
     <div className="flex-1">
     <input type="checkbox" id="checkbox" name="checkbox" value={formValues.checkbox} onChange={handleChange} />
     
     <label htmlFor="checkbox" className="ml-2 text-yellow-300">I agree to the terms and conditions</label>
     </div>
+    {errors.checkbox ? <p className="text-red-700">{errors.checkbox}</p> : <></>}
     
     
-    <button className="mt-5 bg-yellow-400 border border-neutral-500">Sign Up</button>
+    <button className="mt-5 bg-yellow-400 border border-neutral-500" onClick={handleSignUp}>Sign Up</button>
     </div>
     
       
